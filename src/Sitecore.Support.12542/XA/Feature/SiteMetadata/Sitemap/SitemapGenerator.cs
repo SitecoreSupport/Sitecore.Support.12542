e655be7fbecc3c1d38be11dddd4c0b7b4d2765dd
DK\IRO <iro@sitecore.net>
2018-04-18 17:07:08 +0300

    Initial

--- src/Sitecore.Support.12542/XA/Feature/SiteMetadata/Sitemap/SitemapGenerator.cs
+++ src/Sitecore.Support.12542/XA/Feature/SiteMetadata/Sitemap/SitemapGenerator.cs
@@ -1,13 +1,41 @@
 ﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Sitemap
 {
   using Sitecore.Data.Items;
+  using Sitecore.XA.Feature.SiteMetadata.Sitemap;
   using System.Collections.Generic;
+  using System.Collections.Specialized;
+  using System.Text;
+  using System.Threading.Tasks;
   using System.Xml;
 
   public class SitemapGenerator : Sitecore.XA.Feature.SiteMetadata.Sitemap.SitemapGenerator
   {
     public SitemapGenerator() : base() { }
     public SitemapGenerator(XmlWriterSettings xmlWriterSettings) : base(xmlWriterSettings) { }
+
+    public override string GenerateSitemap(Item homeItem, NameValueCollection externalSitemaps, SitemapLinkOptions sitemapLinkOptions)
+    {
+      List<string> externalXmls = null;
+      Task task = null;
+      if (externalSitemaps != null && externalSitemaps.Count > 0)
+      {
+        task = new Task(delegate
+        {
+          DownloadExternalSitemaps(externalSitemaps, out externalXmls);
+        });
+        task.Start();
+      }
+      HashSet<Item> childrenTree = ChildrenSearch(homeItem);
+      StringBuilder stringBuilder = BuildSitemap(childrenTree, sitemapLinkOptions);
+      task?.Wait();
+      if (externalXmls != null && externalXmls.Count > 0)
+      {
+        string value = JoinXmls(stringBuilder.ToString(), externalXmls);
+        stringBuilder.Clear();
+        stringBuilder.Append(value);
+      }
+      return FixEncoding(stringBuilder);
+    }
 
     protected override HashSet<Item> ChildrenSearch(Item homeItem)
     {
